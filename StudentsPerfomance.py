01112a35ce36d1c7720f7b714172c366756d434c
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2021-05-14 09:41:18 +0530

    Update StudentsPerfomance.py

--- StudentsPerfomance.py
+++ StudentsPerfomance.py
@@ -9,7 +9,7 @@ data=df["reading score"].tolist()
 mean=sum(data)/len(data)
 median=statistics.median(data)
 std_deviation=statistics.stdev(data)
-mode=statistics.mode
+mode=statistics.mode(data)
 first_std_deviation_start,first_std_deviation_end=mean-std_deviation,mean+std_deviation
 second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation),mean+(2*std_deviation)
 third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation),mean+(3*std_deviation)
@@ -23,10 +23,10 @@ fig.show()
 list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
 list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
 list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
-print("Mean of this data=".format(mean))
-print("Mode of this data=".format(mode))
-print("Median of this data=".format(median))
-print("Standard Deviation of this data=".format(std_deviation))
-print("{}% of data lies within 1 standard deviation=".format(len(list_of_data_within_1_std_deviation)*100.0/len(data)))
-print("{}% of data lies within 2 standard deviation=".format(len(list_of_data_within_2_std_deviation)*100.0/len(data)))
-print("{}% of data lies within 3 standard deviation=".format(len(list_of_data_within_3_std_deviation)*100.0/len(data)))
+print("Mean of this data is {}".format(mean))
+print("Mode of this data is {}".format(mode))
+print("Median of this data is {}".format(median))
+print("Standard Deviation of this data is {}".format(std_deviation))
+print("{}% of data lies within 1 standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 2 standard deviation".format(len(list_of_data_within_2_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 3 standard deviation".format(len(list_of_data_within_3_std_deviation)*100.0/len(data)))
